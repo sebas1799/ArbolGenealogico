484fea553cf915739df06955db9808b984c9c681
Sebastian Ortiz <49884961+sebas1799@users.noreply.github.com>
2019-07-10 19:19:24 -0500

    Add files via upload

--- ArbolGenealogico.pl
+++ ArbolGenealogico.pl
@@ -1,92 +1,86 @@
 %Padres
-padreDe('Javier','Sebastian').
-padreDe('Amparo','Sebastian').
-
-
+padrede('javier','sebastian').
+padrede('amparo','sebastian').
 
 %Abuelos Maternos
 
-padreDe('Helena','Amparo').
-padreDe('Luis','Amparo').
-padreDe('Helena','Teresa').
-padreDe('Luis','Teresa').
-padreDe('Helena','Janeth').
-padreDe('Luis','Janeth').
-padreDe('Helena','Lizeth').
-padreDe('Luis','Lizeth').
-padreDe('Helena','Andrea').
-padreDe('Luis','Andrea').
+padrede('helena','amparo').
+padrede('luis','amparo').
+padrede('helena','teresa').
+padrede('luis','teresa').
+padrede('helena','janeth').
+padrede('luis','janeth').
+padrede('helena','lizeth').
+padrede('luis','lizeth').
+padrede('helena','andrea').
+padrede('luis','andrea').
 
 %Abuelo Paternos
 
-padreDe('Jose','Javier').
-padreDe('Betty','Javier').
-padreDe('Jose','Yuvely').
-padreDe('Betty','Yuvely').
+padrede('jose','javier').
+padrede('betty','javier').
+padrede('jose','yuvely').
+padrede('betty','yuvely').
 
 %Bisabuelos Maternos
-padreDe('Patrocinio','Helena').
-padreDe('Limbania','Helena').
-padreDe('Patrocinio','Cecilia').
-padreDe('Limbania','Cecilia').
-padreDe('Patrocinio','Elisa').
-padreDe('Limbania','Elisa').
-padreDe('Patrocinio','Oliverio').
-padreDe('Limbania','Oliverio').
-padreDe('Patrocinio','Edelmira').
-padreDe('Limbania','Edelmira').
-padreDe('Patrocinio','Carmen').
-padreDe('Limbania','Carmen').
-padreDe('Patrocinio','Jaime').
-padreDe('Limbania','Jaime').
+padrede('patrocinio','helena').
+padrede('limbania','helena').
+padrede('patrocinio','cecilia').
+padrede('limbania','cecilia').
+padrede('patrocinio','elisa').
+padrede('limbania','elisa').
+padrede('patrocinio','oliverio').
+padrede('limbania','oliverio').
+padrede('patrocinio','edelmira').
+padrede('limbania','edelmira').
+padrede('patrocinio','carmen').
+padrede('limbania','carmen').
+padrede('patrocinio','jaime').
+padrede('limbania','jaime').
 
-
-padreDe('Fidel','Luis').
-padreDe('Salud','Luis').
-padreDe('Fidel','Samuel').
-padreDe('Salud','Samuel').
-padreDe('Fidel','Antonio').
-padreDe('Salud','Antonio').
-padreDe('Fidel','Anita').
-padreDe('Salud','Anita').
+padrede('fidel','luis').
+padrede('salud','luis').
+padrede('fidel','samuel').
+padrede('salud','samuel').
+padrede('fidel','antonio').
+padrede('salud','antonio').
+padrede('fidel','anita').
+padrede('salud','anita').
 
 %Bisabuelos Paternos
-padreDe('Tita','Betty').
-padreDe('Pedro','Betty').
-padreDe('Tita','Yolanda').
-padreDe('Pedro','Yolanda').
-padreDe('Tita','Marina').
-padreDe('Pedro','Marina').
+padrede('tita','betty').
+padrede('pedro','betty').
+padrede('tita','yolanda').
+padrede('pedro','yolanda').
+padrede('tita','marina').
+padrede('pedro','marina').
 
-padreDe('Alberto','Jose').
-padreDe('Maria','Jose').
-padreDe('Alberto','Oscar').
-padreDe('Maria','Oscar').
-padreDe('Alberto','Esperanza').
-padreDe('Maria','Esperanza').
-
+padrede('alberto','jose').
+padrede('maria','jose').
+padrede('alberto','oscar').
+padrede('maria','oscar').
+padrede('alberto','esperanza').
+padrede('maria','esperanza').
 
 
 %Tios y primos
 
-padreDe('Yuvely','Laura').
-padreDe('Jesus','Laura').
-padreDe('Yuvely','Salome').
-padreDe('Jesus','Salome').
-padreDe('Yuvely','Sara').
-padreDe('Jesus','Sara').
+padrede('yuvely','laura').
+padrede('jesus','laura').
+padrede('yuvely','salome').
+padrede('jesus','salome').
+padrede('yuvely','sara').
+padrede('jesus','sara').
 
 
 %Reglas
+hijode(A,B) :- padrede(B,A).
+abuelode(A,B) :- padrede(A,C), padrede(C,B).
+hermanode(A,B) :- padrede(C,A), padrede(C,B), A \== B.
+tiode(A,B) :- hermanode(A,C), hijode(B,C).
+primo(A,B) :- tiode(C,A), hijode(B,C).
 
-hermanoDe(A,B) :-padreDe(C,A), padreDe(C,B), A \== B.
-
-tioDe(A,B) :- hermanoDe(A,C), hijoDe(B,C).
-primo(A,B) :- tioDe(C,A), hijoDe(B,C).
-hijoDe(A,B) :- padreDe(B,A).
-abueloDe(A,B) :-padreDe(A,C), padreDe(C,B).
-
-casado(A,B) :- hijoDe(C,A), hijoDe(C,B), A \== B.
-nietoDe(A,B) :- padreDe(B,C), padreDe(C,A).
+casado(A,B) :- hijode(C,A), hijode(C,B), A \== B.
+nietode(A,B) :- padrede(B,C), padrede(C,A).
 
 feliz(A) :- (casado(A,B)), A \== B.
